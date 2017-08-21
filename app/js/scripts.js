f761e25f3b1e7c2b28e6525e59642855015ef09b
sashka <kamenskogo@gmail.com>
2017-08-21 21:56:16 +0300

    do safari hacks

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,19 +1,26 @@
 'use strict';
-;(function(){
+;(function () {
     $(document).ready(function () {
-        var triggered = false;
+
+        var isIos = /(iPad|iPhone|iPod)/g.test(navigator.userAgent);      // побеждаем шрифты в ios
+        if (isIos) {
+            $(".moneyCounter__money").css("letter-spacing", "-1px");
+            $(".fineInput__input_submit").css("letter-spacing", "-1.5px");
+        }
+
+        var modalOn = false;
         $("body").on("mouseleave", function (e) {
-            if (e.offsetY - $(window).scrollTop() < 0 && !triggered) {
-                triggered = true;
-                $(".modalWindow").fadeIn("slow");
+            if (e.offsetY - $(window).scrollTop() < 0 && !modalOn) {
+                modalOn = true;
+                $(".modalWindow").css("padding-right", "15px").fadeIn("slow");
                 $("body").addClass("modalOn");
                 window.scrollTo(0, 0);
             }
         });
         $(".close").on("click", function () {
-            $(".modalWindow").fadeOut("fast");
+            $(".modalWindow").css("padding-right", "0").fadeOut("fast");
             $("body").removeClass("modalOn");
-            triggered = false;
+            modalOn = false;
         })
     });
 })();
